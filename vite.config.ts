da330ff870aaedb34748dc3b76f1eb8f930f10a3
刘洋 <liuyang@gyhl.com>
2024-06-26 11:54:45 +0800

    feat(init): 优化导入设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-															build: {
-																outDir: "dist", // 自定义构建输出目录
-																target: "es2020",
-																lib: {
-																	entry: "src/main.ts", // 入口文件路径
-																	formats: ["es", "cjs"]
-																}
-															},
-															server: {
-																port: 8080 // 自定义开发服务器端口
-															}
-														});
+  build: {
+    outDir: 'dist', // 自定义构建输出目录
+    target: 'es2020',
+    lib: {
+      entry: 'src/index.ts', // 入口文件路径
+      formats: ['es', 'cjs'],
+      name: 'ly-public-utils',
+      fileName: 'index'
+    }
+  },
+  server: {
+    port: 8080 // 自定义开发服务器端口
+  }
+});
