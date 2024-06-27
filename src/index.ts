800c64254939927f659c184fefcf99b0f3ef427e
刘洋 <liuyang@gyhl.com>
2024-06-27 15:09:48 +0800

    docs(utils): 修改index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ import localStorage from './utils/local.cache.utils';
 import defaultUtils from './utils/default.utils';
 import { deepCopy, flattenArray, buildTree } from './utils/array.format.utils';
 
-defaultUtils.isName('123');
 export { formatDate, Base64, localStorage, defaultUtils, deepCopy, flattenArray, buildTree };
