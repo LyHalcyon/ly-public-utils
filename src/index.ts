3b54d2b133a3fd2863e2b29dd4c602a6feef7407
刘洋 <liuyang@gyhl.com>
2024-06-28 15:32:52 +0800

    feat(export.utils.ts): 添加添加导出excel

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,15 @@ import Base64 from './utils/base64.utils';
 import localStorage from './utils/local.cache.utils';
 import defaultUtils from './utils/default.utils';
 import { deepCopy, flattenArray, buildTree } from './utils/array.format.utils';
+import { exportExcel } from './utils/export.utils';
 
-export { formatDate, Base64, localStorage, defaultUtils, deepCopy, flattenArray, buildTree };
+export {
+  formatDate,
+  Base64,
+  localStorage,
+  defaultUtils,
+  deepCopy,
+  flattenArray,
+  buildTree,
+  exportExcel
+};
